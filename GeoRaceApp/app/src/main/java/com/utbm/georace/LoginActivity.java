7d84a65f47abe6376856be025a6671dbdfd05d61
meskale <johannystrugala@free.fr>
2014-10-13 16:33:57 +0200

    activité principale

--- GeoRaceApp/app/src/main/java/com/utbm/georace/LoginActivity.java
+++ GeoRaceApp/app/src/main/java/com/utbm/georace/LoginActivity.java
@@ -7,6 +7,7 @@ import android.app.Activity;
 import android.app.LoaderManager.LoaderCallbacks;
 import android.content.ContentResolver;
 import android.content.CursorLoader;
+import android.content.Intent;
 import android.content.Loader;
 import android.database.Cursor;
 import android.net.Uri;
@@ -287,7 +288,10 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
             showProgress(false);
 
             if (success) {
+                Intent intent = new Intent(getApplication(), MainActivity.class);
+                startActivity(intent);
                 finish();
+
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
