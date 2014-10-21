c379b8005febcf42384577c45564599d1c67157e
meskale <johannystrugala@free.fr>
2014-10-21 16:05:11 +0200

    ajout des fichiers images et documentation

--- GeoRaceApp/app/src/main/java/com/utbm/georace/LoginActivity.java
+++ GeoRaceApp/app/src/main/java/com/utbm/georace/LoginActivity.java
@@ -290,7 +290,6 @@ public class LoginActivity extends Activity implements LoaderCallbacks<Cursor> {
             if (success) {
                 Intent intent = new Intent(getApplication(), MainActivity.class);
                 startActivity(intent);
-                finish();
 
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
