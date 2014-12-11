6766bb493e692e746117237229cffec9bee86ed6
Franck <franckschn@gmail.com>
2014-12-11 11:03:05 +0100

    commit etrange
    Github pour mac pense que j’ai modifié tous les fichiers

--- GeoRaceApp/app/src/main/java/com/utbm/georace/fragment/RaceMap.java
+++ GeoRaceApp/app/src/main/java/com/utbm/georace/fragment/RaceMap.java
@@ -8,13 +8,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
 import com.utbm.georace.R;
 
 /**
@@ -26,14 +19,7 @@ import com.utbm.georace.R;
  * create an instance of this fragment.
  *
  */
-public class RaceMap extends Fragment{
-
-    static final LatLng HAMBURG = new LatLng(53.558, 9.927);
-    static final LatLng KIEL = new LatLng(53.551, 9.993);
-    private GoogleMap map;
-
-
-
+public class RaceMap extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -78,29 +64,6 @@ public class RaceMap extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-
-        map = ((SupportMapFragment) getFragmentManager().findFragmentById(R.id.map))
-                .getMap();
-
-        Marker hamburg = map.addMarker(new MarkerOptions().position(HAMBURG)
-                .title("Hamburg"));
-        Marker kiel = map.addMarker(new MarkerOptions()
-                .position(KIEL)
-                .title("Kiel")
-                .snippet("Kiel is cool")
-                .icon(BitmapDescriptorFactory
-                        .fromResource(R.drawable.ic_launcher)));
-
-        // Move the camera instantly to hamburg with a zoom of 15.
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(HAMBURG, 15));
-
-        // Zoom in, animating the camera.
-        map.animateCamera(CameraUpdateFactory.zoomTo(10), 2000, null);
-
-
-
-
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_race_map, container, false);
     }
